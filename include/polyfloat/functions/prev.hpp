76c2277951362ac1ea8ceaab6572addedcf101c0
jtlap <jtlapreste@gmail.com>
2026-07-20 22:22:04 +0200

    typo in operator>

--- include/polyfloat/functions/prev.hpp
+++ include/polyfloat/functions/prev.hpp
@@ -80,6 +80,7 @@ namespace plf::_
   template<typename Z, eve::callable_options O>
   POLYFLOAT_FORCEINLINE constexpr auto prev_(POLYFLOAT_DELAY(), O const& , Z const& z) noexcept
   {
+    return -next(-z);
     if constexpr(dimension_v<Z> == 1)
       return eve::prev(z);
     else if constexpr(dimension_v<Z> == 2)
