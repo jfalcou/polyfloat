74a7d03b7ad12068c3555a2f186e4a09402fdd9c
Joel Falcou <joel.falcou@lri.fr>
2026-07-31 17:36:25 +0200

    Fix constant calls

--- include/polyfloat/constants/wrapped.hpp
+++ include/polyfloat/constants/wrapped.hpp
@@ -36,9 +36,9 @@ namespace plf::_
 
     template<concepts::polyfloat T>
     POLYFLOAT_FORCEINLINE constexpr T operator()(as<T> const& v) const
-      requires(requires(base_callable const& b) { b(plf::as_component_type_t<T>{}); })
+      requires(requires(base_callable const& b) { b(plf::as_component<T>{}); })
     {
-      return T{base_callable::operator()(as_component_type_t<T>{})};
+      return T{base_callable::operator()(as_component<T>{})};
     }
 
     POLYFLOAT_CALLABLE_OBJECT(constant_t, constant_);
