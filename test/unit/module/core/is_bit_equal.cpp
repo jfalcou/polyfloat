f0042fa96c353f4c0e26b6e2ec101cc9c1f25b87
jtlap <jtlapreste@gmail.com>
2026-08-19 13:54:07 +0200

    is_bit_equal

---
+++ test/unit/module/core/is_bit_equal.cpp
@@ -0,0 +1,41 @@
+//======================================================================================================================
+/*
+ POLYFLOAT - Extended precision floating points
+ Copyright : POLYFLOAT Contributors & Maintainers
+ SPDX-License-Identifier: BSL-1.0
+*/
+//======================================================================================================================
+#include "test.hpp"
+#include <polyfloat/polyfloat.hpp>
+
+TTS_CASE_WITH("Check add two params",
+              plf::scalar_real_types,
+              tts::randoms(eve::valmin, eve::valmax),
+              tts::randoms(eve::valmin, eve::valmax),
+              tts::randoms(eve::valmin, eve::valmax),
+              tts::randoms(eve::valmin, eve::valmax),
+              tts::randoms(eve::valmin, eve::valmax),
+              tts::randoms(eve::valmin, eve::valmax)
+
+             )
+  <typename T>(T const& a0, T const& a1, T const& a2,
+               T const& a3, T const& a4, T const& a5)
+{
+  using  mpfr::mpreal;
+  using plf::is_bit_equal;
+  using plf::is_equal;
+  {
+    using pv_t  = plf::polyfloat<T, 2>;
+    pv_t pa(a0, a1);
+    pv_t pb(a3, a4);
+    TTS_EQUAL(is_bit_equal(pa, pb), is_equal(pa, pb));
+    TTS_EQUAL(is_bit_equal(pa, pa), is_equal(pa, pa));
+  }
+  {
+    using pv_t  = plf::polyfloat<T, 3>;
+    pv_t pa(a0, a1, a2);
+    pv_t pb(a3, a4, a5);
+    TTS_EQUAL(is_bit_equal(pa, pb), is_equal(pa, pb));
+    TTS_EQUAL(is_bit_equal(pa, pa), is_equal(pa, pa));
+  }
+};
