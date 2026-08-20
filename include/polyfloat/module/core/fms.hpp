79d9763c682f2b4d68466fe9e3d5a5b9ff1aa5e0
jtlap <jtlapreste@gmail.com>
2026-08-20 15:08:14 +0200

    fma family

---
+++ include/polyfloat/module/core/fms.hpp
@@ -0,0 +1,82 @@
+//======================================================================================================================
+/*
+ POLYFLOAT - Extended precision floating points
+ Copyright : POLYFLOAT Contributors & Maintainers
+ SPDX-License-Identifier: BSL-1.0
+*/
+//======================================================================================================================
+#pragma once
+
+#include <polyfloat/details/callable.hpp>
+#include <polyfloat/types/concepts.hpp>
+#include <polyfloat/types/traits.hpp>
+#include <type_traits>
+
+namespace plf
+{
+
+  template<typename Options> struct fms_t : eve::strict_tuple_callable<fms_t, Options, raw_option, pedantic_option>
+  {
+    template<typename... Ts> struct result : as_polyfloat_like<Ts...>
+    {
+    };
+
+    template<concepts::polyfloat_like Z0, concepts::polyfloat_like Z1, concepts::polyfloat_like Z2>
+    EVE_FORCEINLINE typename result<Z0, Z1, Z2>::type constexpr operator()(Z0 z0, Z1 z1, Z2 z2) const noexcept
+    {
+      return POLYFLOAT_CALL(z0, z1, z2);
+    }
+
+
+    POLYFLOAT_CALLABLE_OBJECT(fms_t, fms_);
+  };
+  //======================================================================================================================
+  //! @fmstogroup core
+  //! @{
+  //!   @var fms
+  //!   @brief return the fused multiply substract of the parameters.
+  //!
+  //!   @groupheader{Header file}
+  //!
+  //!   @code
+  //!   #include <polyfloat/core.hpp>
+  //!   @endcode
+  //!
+  //!   @groupheader{Callable Signatures}
+  //!
+  //!   @code
+  //!   namespace polyfloat
+  //!   {
+  //!      template<polyfloat::concepts::polyfloat_like Z1, polyfloat_like Z2, polyfloat_like Z3> constexpr auto fms(Z1 z1, Z2 z2, Z3 z3) noexcept;
+  //!   }
+  //!   @endcode
+  //!
+  //!   **Parameters**
+  //!
+  //!     * `z1`, `z2`, `z3`: Values to process.
+  //!
+  //!   **Return value**
+  //!
+  //!     Returns the fused  multiply substract of the arguments.
+  //!
+  //! @note contrarily to `eve::fms`,  `plf::fms` garanties the correct fused multiply add rounding behaviour,  even if it has to be emulated.
+  //!
+  //!  @groupheader{Example}
+  //!
+  //!  @godbolt{doc/core/fms.cpp}
+  //======================================================================================================================
+
+  inline constexpr auto fms = eve::functor<fms_t>;
+  //======================================================================================================================
+  //! @}
+  //======================================================================================================================
+}
+
+namespace plf::_
+{
+  template<typename Z1, typename Z2, typename Z3, eve::callable_options O>
+  POLYFLOAT_FORCEINLINE constexpr auto fms_(POLYFLOAT_DELAY(), O const& , Z1 const& x, Z2 const& y, Z3 const& z) noexcept
+  {
+    return plf::fma(x, y, -z);
+  }
+}
