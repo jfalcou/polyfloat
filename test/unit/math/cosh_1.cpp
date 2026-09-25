b536188010229bbdddbc9e7d270ceb3d89dcdf30
jtlap <jtlapreste@gmail.com>
2026-09-25 21:57:08 +0200

    cosh_1

--- test/unit/math/cosh_1.cpp
+++ test/unit/math/cosh_1.cpp
@@ -13,7 +13,7 @@ TTS_CASE_TPL("Check cosh_1", plf::scalar_real_types)
 {
   using mpfr::mpreal;
   using plf::cosh_1;
-  auto mcosh_1 = []<typename TT>(TT) { return (mpfr::mpreal::exp(1) + mpfr::mpreal::exp(-1)) / 2; };
+  auto mcosh_1 = []<typename TT>(TT) { return mpfr::cosh(1); };
   {
     {
       TTS_EQUAL(cosh_1(eve::as<T>()), eve::cosh_1(eve::as<T>()));
